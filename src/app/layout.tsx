b9c809d4f461474f03d072fd3591af10bab24aca
Vasiliy Igonin <vigonin83@gmail.com>
2025-09-01 21:29:54 +0300

    init project

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const interSans = Inter({
+  variable: "--font-inter-sans",
+  subsets: ["latin"],
+});
+
+
+export const metadata: Metadata = {
+  title: "Design Studio | Gallery",
+  description: "Design studio site",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${interSans.variable}  antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
